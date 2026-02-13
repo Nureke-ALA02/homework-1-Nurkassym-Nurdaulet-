b23939e00f37e174626d050e5eed975725aea9c2
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-02-13 21:58:07 +0500

    create ranger factory

--- src/main/java/com/narxoz/rpg/factory/RangerFactory.java
+++ src/main/java/com/narxoz/rpg/factory/RangerFactory.java
@@ -1,4 +1,19 @@
 package com.narxoz.rpg.factory;
 
-public class RangerFactory {
+import com.narxoz.rpg.equipment.Armor;
+import com.narxoz.rpg.equipment.Weapon;
+import com.narxoz.rpg.ranger.RangerArmor;
+import com.narxoz.rpg.ranger.RangerWeapon;
+
+public class RangerFactory implements EquipmentFactory {
+
+    @Override
+    public Weapon createWeapon() {
+        return new RangerWeapon();
+    }
+
+    @Override
+    public Armor createArmor() {
+        return new RangerArmor();
+    }
 }
